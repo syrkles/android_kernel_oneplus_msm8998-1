97d000a621c44056fc8bcb1ba50c31e1ea435260
syrkles <syrkles@gmail.com>
2018-09-10 04:59:59 +0000

    Revert "cpufreq: interactive: Add Support to Display State"
    This reverts commit 97537cd6f7cbf093eb5d67d032da8baaa2631503.

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -31,7 +31,6 @@
 #include <linux/workqueue.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
-#include <linux/display_state.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/cpufreq_interactive.h>
